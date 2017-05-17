be87796161e62f2c7a100cc16fe3fea32ee5cbde
Steven Oud <soud@protonmail.com>
2017-05-17 12:32:07 +0200

    Add version and optional dependencies

--- Formula/bnl-platform-command.rb
+++ Formula/bnl-platform-command.rb
@@ -2,11 +2,13 @@ class BnlPlatformCommand < Formula
   desc "Platform related bnl commands"
   homepage "https://github.com/blendle/bnl-platform-command"
   url "https://github.com/blendle/bnl-platform-command.git",
-    :revision => "TODO"
-  version "TODO"
+    :revision => "dd37d3620eed01fa66277680ba6c514b0035a393"
+  version "v.0.1.0"
   head "https://github.com/blendle/bnl-platform-command.git"
 
   depends_on "blendle/blendle/bnl" # questionable, it works without, but not as good
+  depends_on "kubectl" => :optional
+  depends_on "gcloud" => :optional
 
   def install
     bin.install "./bnl-platform-command"
