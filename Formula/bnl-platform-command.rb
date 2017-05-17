20c91466ca678e53961836aa4c72e9ab3a206e5a
Steven Oud <soud@protonmail.com>
2017-05-17 16:38:01 +0200

    Upgrade bnl and bnl-platform-command

--- Formula/bnl-platform-command.rb
+++ Formula/bnl-platform-command.rb
@@ -2,10 +2,10 @@ class BnlPlatformCommand < Formula
   desc "Platform related bnl commands"
   homepage "https://github.com/blendle/bnl-platform-command"
   url "git@github.com:blendle/bnl-platform-command.git",
-    version: "v0.1.0",
-    revision: "ddccf75f6980bd36142fecadf04c6d234ff4d560",
+    version: "v0.2.0",
+    revision: "7adc721e2c4b89a8db5686ef91e515f87dafa6d0",
     using: :git
-  version "ddccf75f6980bd36142fecadf04c6d234ff4d560"
+  version "7adc721e2c4b89a8db5686ef91e515f87dafa6d0"
   head "git@github.com:blendle/bnl-platform-command.git", using: :git
 
   depends_on "blendle/blendle/bnl" => :recommended
