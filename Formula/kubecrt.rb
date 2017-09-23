d495dd6632189c306152b82258af10e510861dc0
Jean Mertz <jean@mertz.fm>
2017-09-23 10:36:06 +0200

    update kubecrt to 0.6.0

--- Formula/kubecrt.rb
+++ Formula/kubecrt.rb
@@ -2,29 +2,26 @@ class Kubecrt < Formula
   desc "Convert Helm charts to Kubernetes resources."
   homepage "https://github.com/blendle/kubecrt"
   url "https://github.com/blendle/kubecrt.git",
-    :tag => "v0.5.1",
-    :revision => "a591b237b3621446ac0b383ecb62cddd42315e4c"
+    :tag => "v0.6.0",
+    :revision => "7e2d38e076b3b6fd43d05b74096316dbee47cf72"
   head "https://github.com/blendle/kubecrt.git"
-
-  depends_on "go" => :build
-  depends_on "glide" => :build
 
   bottle do
     root_url "https://homebrew-blendle.storage.googleapis.com"
     cellar :any_skip_relocation
-    sha256 "9e1da44221d6ce25d8f0c2e88cb633fd1e1da99e75034f631d7e82ea1ea2a337" => :sierra
+    sha256 "7bb5ef0a916cd4056b23f61edc6e12516395d9d8c9593e5dd5a4e845aeeacbf4" => :high_sierra
   end
+
+  depends_on "dep" => :build
+  depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = buildpath
-    ENV["GLIDE_HOME"] = HOMEBREW_CACHE/"glide_home/#{name}"
-    dir = buildpath/"src/github.com/blendle/kubecrt"
-    dir.install Dir["*"]
-    cd dir do
-      system "script/bootstrap"
-      system "go", "build", "-o", bin/"kubecrt",
-      "-ldflags",
-      "-X github.com/blendle/kubecrt/config.version=#{version} -X github.com/blendle/kubecrt/config.gitrev=a591b23"
+    buildpath.join("src/github.com/blendle/kubecrt").install buildpath.children
+    cd "src/github.com/blendle/kubecrt" do
+      system "dep", "ensure", "-vendor-only"
+      system "make", "build"
+      bin.install "bin/kubecrt"
     end
   end
 
