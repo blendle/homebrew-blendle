5167204d9d562a99e11276559a351840dc05a9e3
Jean Mertz <jean@mertz.fm>
2017-12-27 19:25:24 +0100

    update kubecrt to 0.6.1

--- Formula/kubecrt.rb
+++ Formula/kubecrt.rb
@@ -2,14 +2,14 @@ class Kubecrt < Formula
   desc "Convert Helm charts to Kubernetes resources."
   homepage "https://github.com/blendle/kubecrt"
   url "https://github.com/blendle/kubecrt.git",
-    :tag => "v0.6.0",
-    :revision => "7e2d38e076b3b6fd43d05b74096316dbee47cf72"
+    :tag => "v0.6.1",
+    :revision => "6cd24196f610a98d0d197d8d7cefb250e3cd63a7"
   head "https://github.com/blendle/kubecrt.git"
 
   bottle do
     root_url "https://homebrew-blendle.storage.googleapis.com"
     cellar :any_skip_relocation
-    sha256 "7bb5ef0a916cd4056b23f61edc6e12516395d9d8c9593e5dd5a4e845aeeacbf4" => :high_sierra
+    sha256 "8a43e06d80b873d58b9de584997c7a7362a9420fb3c8032c9b7529fd7aff37ec" => :high_sierra
   end
 
   depends_on "dep" => :build
