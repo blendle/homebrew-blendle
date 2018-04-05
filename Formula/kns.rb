e430c5dd98aeea2e7c221a374bd9b7e20af34d0f
Koen Bollen <meneer@koenbollen.nl>
2018-04-05 10:30:05 +0200

    Update `kns` to the latest master

--- Formula/kns.rb
+++ Formula/kns.rb
@@ -1,8 +1,8 @@
 class Kns < Formula
   desc "quick Kubernetes Namespace Switcher"
   homepage "https://github.com/blendle/kns"
-  url "https://github.com/blendle/kns.git", :revision => "1126226c46004694cb472a73731eafae071d6016"
-  version "1126226c46004694cb472a73731eafae071d6016"
+  url "https://github.com/blendle/kns.git", :revision => "ac3d5bdb3fc5ad94899b2cc278682c7eba9f3deb"
+  version "ac3d5bdb3fc5ad94899b2cc278682c7eba9f3deb"
   head "https://github.com/blendle/kns.git"
 
   depends_on "fzf"
