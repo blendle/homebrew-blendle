c32f0dcbbc6b3cb1bf6ad09b3d10065d306b8fed
Koen Bollen <meneer@koenbollen.nl>
2018-04-10 10:39:23 +0200

    Update kns and include the ktx command.

--- Formula/kns.rb
+++ Formula/kns.rb
@@ -1,15 +1,17 @@
 class Kns < Formula
   desc "quick Kubernetes Namespace Switcher"
   homepage "https://github.com/blendle/kns"
-  url "https://github.com/blendle/kns.git", :revision => "ac3d5bdb3fc5ad94899b2cc278682c7eba9f3deb"
-  version "ac3d5bdb3fc5ad94899b2cc278682c7eba9f3deb"
+  url "https://github.com/blendle/kns.git", :revision => "9e30d8df96dc864383845ea5a12cd460f33166fa"
+  version "9e30d8df96dc864383845ea5a12cd460f33166fa"
   head "https://github.com/blendle/kns.git"
 
   depends_on "fzf"
   depends_on "kubernetes-cli" => :optional
 
   def install
-    bin.install "./kns"
+    bin.install "bin/kns"
+    bin.install "bin/ktx"
+    prefix.install "helpers"
   end
 
   def caveats; <<~EOS
