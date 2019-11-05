9c7fed8232770c8e5e93eb3e1ea844d081633a0b
Niels Stevens <niels@blendle.com>
2019-11-05 11:14:16 +0100

    Update Epp to version 3.0.0

--- Formula/epp.rb
+++ Formula/epp.rb
@@ -2,14 +2,14 @@ class Epp < Formula
   desc "Small templating engine that allows the use of environmental variables."
   homepage "https://github.com/blendle/epp"
   url "https://github.com/blendle/epp.git",
-      :tag => "v2.2.0",
-      :revision => "9a543b2ee47994dcd6e0d1021eace0c458acf4f2"
+    :tag => "v3.0.0",
+    :revision => "e3a91c4a56070ee6da295f0390b85426af138351"
   head "https://github.com/blendle/epp.git"
 
   bottle do
     root_url "https://homebrew-blendle.storage.googleapis.com"
     cellar :any_skip_relocation
-    sha256 "3f1f2cd20a6ede9410103c63c77c7b061d20a90ea87b58137f3d845a228c61d8" => :mojave
+    sha256 "210939057c89dd70d71d802e563616e28088b2a5fc052db1239ab62ea5e723e2" => :catalina
   end
 
   depends_on "go" => :build
