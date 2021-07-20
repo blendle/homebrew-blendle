8a4b8674e597f530fe1cab9364f18aa83fabf9aa
Oscar Edel <edeloscar@gmail.com>
2021-07-20 15:46:14 +0200

    Update kubecrt version to v0.9.1

--- Formula/kubecrt.rb
+++ Formula/kubecrt.rb
@@ -2,13 +2,13 @@ class Kubecrt < Formula
   desc "Convert Helm charts to Kubernetes resources"
   homepage "https://github.com/blendle/kubecrt"
   url "https://github.com/blendle/kubecrt.git",
-    tag:      "v0.9.0",
-    revision: "aa3c0eb7aac3b8a98c4a5b80729398a74a78fb84"
+    tag:      "v0.9.1",
+    revision: "56be1141b406552ed043c29a87950cd519f7bbc6"
   head "https://github.com/blendle/kubecrt.git"
 
   bottle do
     root_url "https://homebrew-blendle.storage.googleapis.com"
-    cellar :any_skip_relocation
+    sha256 cellar: :any_skip_relocation
     sha256 "18f2f3a15a9b17c5e12076752e4050a8200db8b9692a1aa3928be5ebad87a4a4" => :high_sierra
   end
 
