84d6593b8e27bc804340a16be7b26fdd3cb78576
Oscar Edel <edeloscar@gmail.com>
2021-07-28 13:58:44 +0200

    Fix kubecrt Formula style

--- Formula/kubecrt.rb
+++ Formula/kubecrt.rb
@@ -6,13 +6,13 @@ class Kubecrt < Formula
     revision: "84ecbcfd325597e368cca640b734fcae3232b37d"
   head "https://github.com/blendle/kubecrt.git"
 
-  depends_on "go" => :build
-
   bottle do
     root_url "https://homebrew-blendle.storage.googleapis.com"
     rebuild 1
     sha256 cellar: :any_skip_relocation, big_sur: "9ca36d0e8e9570c64b0dd856057331480288ba49257f87ad3b1a80c670107f4a"
   end
+
+  depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = buildpath
